2add28e430b0fc27e557b1e41e837250788b7cba
Gongchaeyeon <66231912+Gongchaeyeon@users.noreply.github.com>
2022-08-02 03:45:20 +0900

    [Bronze I] Title: 팰린드롬수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,21 @@
+import sys
+a = []
+
+while True:
+    b = sys.stdin.readline().strip()
+    if not b:
+        break
+    a.append(b)
+
+for i in range(len(a)):
+    no = 0
+    if (int(a[i][0])==0):
+        continue
+    for j in range(len(a[i])//2):
+        if a[i][j] != a[i][len(a[i])-(j+1)] or int(a[i][0])==0:
+            no+=1
+
+    if no ==0:
+        print("yes")
+    else:
+        print("no")
