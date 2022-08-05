3c1043f0495c0e15655df803b06474552bc63d19
Gongchaeyeon <66231912+Gongchaeyeon@users.noreply.github.com>
2022-08-05 17:54:36 +0900

    [Bronze III] Title: 직각삼각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -2,30 +2,13 @@ import sys
 input = sys.stdin.readline
 
 while True:
-    a,b,c= map(int,input().split())
-    if (a==0 or b==0 or c==0) :
+    Nlist= list(map(int,input().split()))
+
+    if (Nlist[0]==0 or Nlist[1]==0 or Nlist[2]==0) :
         break;
-    max = a
+    NList = sorted(Nlist,reverse=True)
 
-    if a<b:
-        max = b
-    if max<c:
-        max = c
-    # print(max)
-    if max == a:
-        if b**2 + c**2 == max**2:
-            print("right")
-        else:
-            print("wrong")
-    elif max == b:
-        if a**2 + c**2 == max**2:
-            print("right")
-        else:
-            print("wrong")
-    elif max == c:
-        if b**2 + a**2 == max**2:
-            print("right")
-        else:
-            print("wrong")
-
-
+    if(NList[0]**2 == NList[1]**2 + NList[2]**2):
+        print("right")
+    else:
+        print("wrong")
