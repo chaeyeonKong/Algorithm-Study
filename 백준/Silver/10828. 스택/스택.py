c8cd56e5f06678f7f1fb433abb7214bad65fe14f
Gongchaeyeon <66231912+Gongchaeyeon@users.noreply.github.com>
2022-08-19 19:51:17 +0900

    [Silver IV] Title: 스택, Time: 428 ms, Memory: 32460 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,38 @@
+import sys , collections
+input = sys.stdin.readline
+
+N = int(input())
+arr=[]
+
+for i in range(N):
+    order=input()[:-1]
+    if order[:4] == 'push':
+        num = int(order[5:])
+        arr = arr[::-1]
+        arr.append(num)
+        arr = arr[::-1]
+    elif order=='pop':
+        if len(arr)!=0:
+            arr = arr[::-1]
+            print(arr[len(arr)-1])
+            arr.pop()
+            arr = arr[::-1]
+        else:
+            print(-1)
+    elif order=='size':
+        print(len(arr))
+    elif order=='empty':
+        if len(arr)==0:
+            print(1)
+        else:
+            print(0)
+    elif order=='front':
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr[len(arr)-1])
+    elif order=='top':
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr[0])
