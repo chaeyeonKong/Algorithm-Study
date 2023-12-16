69e38d9c0520ae5f77f35a410dac502ba318a4de
Gongchaeyeon <66231912+Gongchaeyeon@users.noreply.github.com>
2023-12-17 07:05:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 5064 ms, Memory: 229084 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,17 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+
+arr=[[0]*(k + 1)for _ in range(n + 1)]
+ret = arr[0][0]
+answer = []
+
+for i in range(1, n + 1):
+    size, value = map(int, sys.stdin.readline().split())
+    for j in range(1, k + 1):
+        if j-size >= 0:
+            arr[i][j] = max(arr[i-1][j], arr[i-1][j-size] + value)
+        else:
+            arr[i][j] = arr[i-1][j]
+
+print(arr[n][k])
