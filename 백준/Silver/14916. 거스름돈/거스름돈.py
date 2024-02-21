6cbd0384f3b9b1d4b1cf716db9d259eaa7f198de
Gongchaeyeon <66231912+Gongchaeyeon@users.noreply.github.com>
2024-02-21 22:10:33 +0900

    [Silver V] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,15 +1,15 @@
 import sys
 n = int(sys.stdin.readline().strip())
 count=0
-while(True):
+
+while(n>0):
     if n%5==0:
-        count+= n//5
+        count+=n//5
         break
     else:
         n-=2
         count+=1
-        
-    if n<0: break
+
 if n<0:
     print(-1)
 else:
