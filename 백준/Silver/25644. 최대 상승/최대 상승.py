45ee594d778fd59d7507eb2a22908b5f774cd6da
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2025-01-15 13:32:48 +0900

    [Silver V] Title: 최대 상승, Time: 152 ms, Memory: 56452 KB -BaekjoonHub

--- 백준/Silver/25644. 최대 상승/최대 상승.py
+++ 백준/Silver/25644. 최대 상승/최대 상승.py
@@ -1,13 +1,12 @@
 import sys
 
 n = int(sys.stdin.readline().strip())
+
 arr = list(map(int,sys.stdin.readline().split()))
 
-minValue = arr[0]
-ans=0
-
-for i in range(1,n):
-    ans = max(ans, arr[i] - minValue)
-    minValue = min(minValue, arr[i])
-
-print(ans)
+mini = arr[0]
+ret = 0
+for i in range(n):
+    ret = max(ret, arr[i]-mini)
+    mini = min(mini, arr[i])
+print(ret)
