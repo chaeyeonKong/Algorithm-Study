6473f12aceabf840a04cd5f5b4f317d6c7cd2dec
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2025-04-02 11:47:56 +0900

    [Silver III] Title: 수리공 항승, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,16 @@
+import sys
+n, l = map(int, sys.stdin.readline().split())
+l-=1
+
+arr = list( map(int, sys.stdin.readline().split()))
+arr = sorted(arr)
+
+tape = 1
+end = arr[0] + l
+
+for i in range(1,n):
+    if end < arr[i]:
+        tape+=1
+        end = arr[i] + l
+
+print(tape)
