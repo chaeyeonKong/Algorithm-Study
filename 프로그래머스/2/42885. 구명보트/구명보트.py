ebcfbf9c327f8dfd2a7ef99f605075f62020b46a
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2025-05-09 17:34:13 +0900

    [level 2] Title: 구명보트, Time: 8.94 ms, Memory: 9.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,18 @@
+def solution(people, limit):
+    start = 0
+    end = len(people)-1
+    answer=0
+    
+    people.sort()
+    
+    while(start <= end):
+        if people[start] + people[end]<=limit:
+            start +=1
+            end-=1
+        else:
+            end-=1
+        answer+=1
+        
+    return answer;
+
+# solution([100, 50, 40, 40, 40, 40], 240)
