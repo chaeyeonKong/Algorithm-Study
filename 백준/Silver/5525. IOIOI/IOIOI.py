0bb2fcadec6f0b036c71119db18f9412f18e924f
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2025-07-03 12:19:10 +0900

    [Silver I] Title: IOIOI, Time: 332 ms, Memory: 41204 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,18 +1,22 @@
 import sys
 
-p = 'I'
-plus = 'OI'
-
 n = int(sys.stdin.readline().strip())
-p+=n*plus
-
 k = int(sys.stdin.readline().strip())
 txt = list(sys.stdin.readline().strip())
 
 cnt = 0
-for i in range(k):
-    if txt[i]=='I':
-        if txt[i:i+len(p)]==list(p):
-            cnt+=1
+i = 0
+pattern = 0
 
+while i<k-1:
+    if txt[i]=='I' and i+2 < k and txt[i+1]=='O' and txt[i+2]=='I':
+        pattern+=1 # IOI 찾을 때 마다 증가
+        if pattern == n:
+            cnt+=1
+            pattern -= 1
+        i+=2 #마지막 I 포함해서 다시 탐색
+    else:
+        pattern = 0
+        i+=1
+        
 print(cnt)
