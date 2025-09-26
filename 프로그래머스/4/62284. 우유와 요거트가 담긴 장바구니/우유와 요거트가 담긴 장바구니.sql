ce2d95d0c04c9008f2f997c0d316db5b7fafa610
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2025-09-26 20:26:09 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,2 +1,4 @@
-
-select distinct a.cart_id from (SELECT * from CART_PRODUCTS where name = 'Milk') a inner join (SELECT * from CART_PRODUCTS where name = 'Yogurt') b on a.cart_id = b.cart_id order by a.cart_id
+select cart_id from cart_products
+group by cart_id
+having max(name='Milk') and max(name='Yogurt')
+order by cart_id
