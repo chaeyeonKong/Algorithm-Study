ba95225339f4bbc8868842144cf5597540fc22c1
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-02-09 20:35:13 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 264 ms, Memory: 53288 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,26 +1,21 @@
 import sys
-n, k = map(int, sys.stdin.readline().split())
+n,k = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
-dict = {}
-ret = []
-idx = 0
 
+dict = {}
 left = 0
 right = 0
-
+ret = []
 for i in range(n):
-    el = arr[right]
-    if el not in dict:
-        dict[el] = 1
+    if arr[right] not in dict:
+        dict[arr[right]]  = 1
     else:
-        dict[el] += 1
+        dict[arr[right]] += 1
 
-    while(dict[el]>k): # 슬라이딩 윈도우가 깨졌을 때 이걸로 left를 이동하는게 핵심
-        if arr[left] in dict:
-            dict[arr[left]]-=1
-        left +=1
-    ret.append(right - left + 1)
+
+    while(dict[arr[right]]>k):
+        dict[arr[left]]-=1
+        left+=1
+    ret.append(right-left+1)
     right+=1
-
-# print(ret)
 print(max(ret))
