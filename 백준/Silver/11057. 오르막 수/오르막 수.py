8dd7915d1d7ac4b937d2b7bda86c6075ebc56470
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-02-10 15:40:04 +0900

    [Silver I] Title: 오르막 수, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline().strip())
+
+dp=[[0]*10 for _ in range(n+1)]
+for i in range(10):
+    dp[0][i] = 1
+
+for i in range(1,n+1):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][j:])
+    # print(dp)
+print(sum(dp[n-1])%10007)
