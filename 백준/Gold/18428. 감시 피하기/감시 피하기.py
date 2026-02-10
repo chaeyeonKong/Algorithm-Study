e262259411d387cdf19499d05c34a626e820ad56
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-02-10 19:30:13 +0900

    [Gold V] Title: 감시 피하기, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,55 +1,53 @@
 import sys
 n = int(sys.stdin.readline().strip())
+
 arr = []
 teacher = []
+flag = False
+
 
 
 def backtraking(cnt):
     global flag
 
-    if cnt==3:
+    if cnt == 3:
         if bfs():
             flag = True
-            return
-    else:
-        for x in range(n):
-            for y in range(n):
-                if arr[x][y] == 'X':
-                    arr[x][y] = 'O'
-                    backtraking(cnt+1)
-                    arr[x][y] = 'X'
+        return
 
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j]=='X':
+                arr[i][j]='O'
+                backtraking(cnt+1)
+                arr[i][j]='X'
 
+def bfs():
+    move=[[-1,0],[1,0],[0,1],[0,-1]]
+    for tx, ty in teacher:
+        for i in range(4):
+            nx = tx
+            ny = ty
+            while(True):
+                nx += move[i][0]
+                ny += move[i][1]
 
-def bfs(): # 이건 장애물을 배치했을 때 감시하는 것
-    move = [[0, 1], [0, -1], [-1, 0], [1, 0]]
-    for tx, ty in teacher: # 선생님의 위치에서
-        for k in range(4): # 상하좌우 탐색
-            xx = tx
-            yy = ty
+                if 0 <= nx < n and 0 <= ny < n:
 
-            while 0 <= xx < n and 0 <= yy < n:
-                if arr[xx][yy]=='O':
+                    if arr[nx][ny]=='O':
+                        break
+                    if arr[nx][ny]=='S':
+                        return False
+                else:
                     break
-                if arr[xx][yy]=='S': # 학생이 보이면 탈락!
-                    return False
-
-                xx += move[k][0]
-                yy += move[k][1]
-
     return True
-
-
-
 
 for i in range(n):
     arr.append(list(map(str, sys.stdin.readline().split())))
-
     for j in range(n):
-        if arr[i][j]=='T':
+        if arr[i][j] == 'T':
             teacher.append([i,j])
 
-flag = False
 backtraking(0)
 
 if flag:
