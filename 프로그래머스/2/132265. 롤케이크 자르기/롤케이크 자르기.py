796453fd93c8834e85d69b3fefd563ff16145e28
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-06-22 15:01:54 +0900

    [level 2] Title: 롤케이크 자르기, Time: 365.43 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,23 +1,23 @@
 def solution(topping):
     cnt=0
-    
-    dict = {}
-    left = set()
+    dict={}
+    li = set()
     for el in topping:
         if el in dict:
             dict[el]+=1
         else:
             dict[el]=1
     
-    for el in topping:
-        left.add(el)
-        if el in dict:
-            if dict[el]==1:
-                del(dict[el])
-            else:
-                dict[el]-=1
-        if len(left) == len(dict):
-            cnt+=1
+    
+    for i in range(len(topping)):
+        target = topping[i]
+        li.add(target)
+        if dict[target]==1:
+            del(dict[target])
+        elif dict[target]>1:
+            dict[target]-=1
             
+        if len(li) == len(dict):
+            cnt+=1
     
     return cnt
