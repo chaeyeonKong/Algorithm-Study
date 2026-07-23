e1869bd524b54544222bc951385dce929af73999
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-07-23 11:07:31 +0900

    [level 2] Title: 택배상자, Time: 68.20 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,44 +1,23 @@
-from collections import deque
+
 
 def solution(order):
-    
-    arr = deque([0 for i in range(len(order))])
-    
-    cnt=1
-    for el in order:
-        arr[el-1] = cnt
-        cnt+=1
         
-    su = 1
-    ans=0
-    s=[]
+    s = []
+    k = 1
+    ans = 0
     
-    while(True):
-        check1=True; check2=True
-        if arr!=deque([]):
-            belt1 = arr.popleft()
-            if belt1==su:
-                ans+=1
-                su+=1
-            elif belt1 != su:
-                s.append(belt1)
-                check1=False
-                continue
+    for target in order:
+        while k < target:
+            s.append(k)
+            k+=1
+        
+        if k == target:
+            ans +=1
+            k+=1
+        elif s and s[-1]==target:
+            s.pop()
+            ans+=1
         else:
-            check1=False
-                
-        if s!=[]:
-            belt2 = s.pop()
-            if belt2 != su:
-                s.append(belt2)
-                check2=False
-            else:
-                su+=1
-                ans+=1
-        else:
-            check2=False
-                
-        if check1 == False and check2 == False:
             break
             
     return ans
